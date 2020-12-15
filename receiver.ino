37a421c705486414c5d85effc28e771211aac6dc
Matia Rašetina <mate32@me.com>
2020-12-15 18:04:11 +0100

    Receiver

---
+++ receiver.ino
@@ -0,0 +1,91 @@
+// MR - receiver v0.1
+#include "setupReceiver.h"
+
+/*
+MR - v1.4 - primanje podataka ->
+TPS - 0x01
+Temp motora - 0x02
+Tlak ulja - 0x03
+Temp ulja - 0x04
+Lamda senzori - 0x05
+RPM - 0x06
+Accelerometar - 0x07
+14 fuseova
+Random adrese - nisu bitne
+*/
+
+//nakon sto se svi podaci dobiju, onda svaku varijablu posebno ispisati
+//testirati na svojoj arduino plocici program za ispisivanje telemetrije
+
+//namjestiti channel za komunikaciju izmedju bolida i receivera !!!!!!! komanda AT+C069 npr
+
+void setup() {
+  Serial.begin(9600);
+  HC12.begin(9600);
+}
+
+void loop() {
+  int tps;
+  int engineTemperature;
+  int oilPressure;
+  int oilTemperature;
+  int lambdaValue;
+  int engineRPM;
+  int accelerometer;
+  
+  while(HC12.available()){
+    unsigned char canID = HC12.read();
+    int value = HC12.read();
+    
+    switch(canID){ 
+            case THROTTLE_POSITION_SENSOR_ID:{
+              tps = value;
+              break;
+              }
+            case ENGINE_TEMPERATURE_ID:{
+              engineTemperature = value;
+              break;
+              }
+            case OIL_PRESSURE_ID:{
+              oilPressure = value;
+              break;
+              }
+            case OIL_TEMPERATURE_ID:{
+              oilTemperature = value;
+              break;
+              }
+            case LAMBDA_SENSOR_ID:{
+              lambdaValue = value;
+            }
+              break;
+            case ENGINE_RPM_ID:{
+              engineRPM = value;
+              break;
+            }
+            case ACCELEROMETER_ID:{
+              accelerometer = value;
+              break;
+              }
+            default:{
+              }
+            }
+
+/* moguci problem
+ *  ako HC12 cijelo vrijeme prima vrijednosti, onda nikad while nece zavrsiti sto znaci
+ *  da se podaci nikad nece printati
+ *  
+ *  nakon testa vidjeti dal moze Serial.print biti izvan whilea tj oce li tako funkcionirati
+ *  ako ne, onda se jednostavno Serial.print ubaci u while i sve bi trebalo bit ok
+ */
+    
+    
+    Serial.print(tps + ", ");
+    Serial.print(engineTemperature + ", ");
+    Serial.print(oilPressure + ", ");
+    Serial.print(oilTemperature + ", ");
+    Serial.print(engineRPM + ", ");
+    Serial.print(accelerometer + ", ");
+    Serial.println(lambdaValue);
+    delay(50);
+  }
+}
---
+++ setupReceiver.h
@@ -0,0 +1,14 @@
+#include <SoftwareSerial.h>
+
+const byte HC12RxdPin = 3;                      // "RXD" Pin on HC12
+const byte HC12TxdPin = 2;                      // "TXD" Pin on HC12
+const byte HC12SetPin = 4;                      // "SET" Pin on HC12
+SoftwareSerial HC12(HC12TxdPin, HC12RxdPin);
+
+#define THROTTLE_POSITION_SENSOR_ID 0x01
+#define ENGINE_TEMPERATURE_ID 0x02
+#define OIL_PRESSURE_ID 0x03
+#define OIL_TEMPERATURE_ID 0x04
+#define LAMBDA_SENSOR_ID 0x05 
+#define ENGINE_RPM_ID 0x06
+#define ACCELEROMETER_ID 0x07
